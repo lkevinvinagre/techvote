158e17e1ed5d0fc96560cb2f5aafdae281bb595c
lkevinvinagre <lkevinvinagre@hotmail.com>
2025-05-29 11:10:33 -0400

    api update

--- api/configs/settings.py
+++ api/configs/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'techvote.urls'
+ROOT_URLCONF = 'configs.urls'
 
 TEMPLATES = [
     {
@@ -66,7 +66,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'techvote.wsgi.application'
+WSGI_APPLICATION = 'configs.wsgi.application'
 
 
 # Database
